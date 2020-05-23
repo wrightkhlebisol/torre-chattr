bb1b563b716e368457d99107d9a52e86e9e2c39d
Customer <u1591-wsa3eam48khj@giow1063.siteground.us>
2020-05-23 16:36:40 +0000

    Fixed server deployment issues

--- resources/views/_comment_section.blade.php
+++ resources/views/_comment_section.blade.php
@@ -1,10 +1,10 @@
 <div class="flex my-5 border-solid border-2 border-gray-200 p-2">
-    <div class="flex items-center text-sm p-2">
+    <div class="flex-1 items-center text-sm p-2">
         <img src="https://i.pravatar.cc/40" alt="{{ $chattr->torre_details_id }}" class="rounded-full mr-2">
     </div>
-    <p class="p-2 lg:text-lg text-xs">{{ $chattr->chattr }}</p>
-    <hr>
-    <div class="flex items-center lg:text-3xl text-sm">
+    <p class="flex-1 p-2 lg:text-lg text-xs">{{ $chattr->chattr }}</p>
+
+    <div class="flex-1 items-center lg:text-3xl text-sm">
         <div class="flex-1 p-2">
             <a href="#up">{{ $chattr->likes }} 👍</a>
         </div>
